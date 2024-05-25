2227b83dcccaa220e8f9390a77bc34f005246b16
Martin Chapman <contact@martinchapman.co.uk>
2024-05-25 22:26:43 +0100

    Update made by Phenoflow

---
+++ python/ace-inhibitors-180mg---primary.py
@@ -0,0 +1,25 @@
+# David A Springate, Darren M Aschroft, Evangelos Kontopantelis, Tim Doran, Ronan Ryan, David Reeves, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"38395","system":"gprdproduct"},{"code":"20579","system":"gprdproduct"},{"code":"12874","system":"gprdproduct"},{"code":"24268","system":"gprdproduct"},{"code":"17689","system":"gprdproduct"},{"code":"16060","system":"gprdproduct"},{"code":"17545","system":"gprdproduct"},{"code":"16161","system":"gprdproduct"},{"code":"3222","system":"gprdproduct"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('ace-inhibitors-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["ace-inhibitors-180mg---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["ace-inhibitors-180mg---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["ace-inhibitors-180mg---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
